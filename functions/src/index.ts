e97382b49cc83632e17024a50d18b24bef93ece7
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2020-10-23 16:03:35 +0700

    Enable CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as cors from "cors";
 
 admin.initializeApp();
 
 const usersCol = admin.firestore().collection("Users");
 const gamesRef = admin.database().ref("games");
+const corsHandler = cors({ origin: true });
 
 // // Start writing Firebase Functions
 
@@ -38,20 +40,22 @@ export const addUserDoc = functions
 export const deleteUserDoc = functions
   .region("asia-southeast2")
   .https.onRequest((req, res) => {
-    //Check if uid passed
-    const uid: any = req.query.uid;
-    if (!uid) sendStatusMsg(400, false, "No uid passed.", res);
+    corsHandler(req, res, () => {
+      //Check if uid passed
+      const uid: any = req.query.uid;
+      if (!uid) sendStatusMsg(400, false, "No uid passed.", res);
 
-    return usersCol
-      .doc(uid)
-      .delete()
-      .then(() => {
-        sendData(200, "uid", uid, res);
-      })
-      .catch((err) => {
-        console.log(err);
-        sendStatusMsg(404, false, "No user doc found with given uid.", res);
-      });
+      return usersCol
+        .doc(uid)
+        .delete()
+        .then(() => {
+          sendData(200, "uid", uid, res);
+        })
+        .catch((err) => {
+          console.log(err);
+          sendStatusMsg(404, false, "No user doc found with given uid.", res);
+        });
+    });
   });
 
 //Required user to authenticate and data as object: username / firstname / lastname
@@ -92,160 +96,180 @@ export const changeUserData = functions
 export const incrementUserScore = functions
   .region("asia-southeast2")
   .https.onRequest((req, res) => {
-    //Check if uid passed
-    const uid: any = req.query.uid;
-    if (!uid) sendStatusMsg(400, false, "No uid passed", res);
+    corsHandler(req, res, () => {
+      //Check if uid passed
+      const uid: any = req.query.uid;
+      if (!uid) sendStatusMsg(400, false, "No uid passed", res);
 
-    const increment = admin.firestore.FieldValue.increment(1);
+      const increment = admin.firestore.FieldValue.increment(1);
 
-    return usersCol
-      .doc(uid)
-      .update({
-        totalGamesWon: increment,
-        gamesWonDay: increment,
-        gamesWonWeek: increment,
-        updatedAt: admin.firestore.Timestamp.now(),
-      })
-      .then(() => {
-        sendStatusMsg(200, true, "User's games won incremented.", res);
-      })
-      .catch((err) => {
-        console.log(err);
-        sendStatusMsg(404, false, "No user found with given uid.", res);
-      });
+      return usersCol
+        .doc(uid)
+        .update({
+          totalGamesWon: increment,
+          gamesWonDay: increment,
+          gamesWonWeek: increment,
+          updatedAt: admin.firestore.Timestamp.now(),
+        })
+        .then(() => {
+          sendStatusMsg(200, true, "User's games won incremented.", res);
+        })
+        .catch((err) => {
+          console.log(err);
+          sendStatusMsg(404, false, "No user found with given uid.", res);
+        });
+    });
   });
 
 //Required uid as query
 export const getUserData = functions
   .region("asia-southeast2")
   .https.onRequest((req, res) => {
-    //Check if uid passed
-    const uid: any = req.query.uid;
-    if (!uid) {
-      sendStatusMsg(400, false, "No uid passed.", res);
-    }
+    corsHandler(req, res, () => {
+      //Check if uid passed
+      const uid: any = req.query.uid;
+      if (!uid) {
+        sendStatusMsg(400, false, "No uid passed.", res);
+      }
 
-    return usersCol
-      .doc(uid)
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          sendData(200, "userData", doc.data(), res);
-        } else {
-          sendStatusMsg(404, false, "No user found with given uid.", res);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        sendStatusMsg(500, false, "Fail to get user document.", res);
-      });
+      return usersCol
+        .doc(uid)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            sendData(200, "userData", doc.data(), res);
+          } else {
+            sendStatusMsg(404, false, "No user found with given uid.", res);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          sendStatusMsg(500, false, "Fail to get user document.", res);
+        });
+    });
   });
 
 //Required numOfPlayers and timeRange (day / week / allTime) as query
 export const getTopScorers = functions
   .region("asia-southeast2")
   .https.onRequest((req, res) => {
-    //Check if numOfPlayers passed
-    let numOfPlayers: any = req.query.numOfPlayers;
-    if (!numOfPlayers)
-      sendStatusMsg(400, false, "No numOfPlayers passed.", res);
-    if (Number(numOfPlayers) % 1 !== 0)
-      sendStatusMsg(400, false, "numOfPlayers is not integer.", res);
+    corsHandler(req, res, () => {
+      //Check if numOfPlayers passed
+      let numOfPlayers: any = req.query.numOfPlayers;
+      if (!numOfPlayers)
+        sendStatusMsg(400, false, "No numOfPlayers passed.", res);
+      if (Number(numOfPlayers) % 1 !== 0)
+        sendStatusMsg(400, false, "numOfPlayers is not integer.", res);
 
-    //Check if timeRange passed and in correct format
-    const timeRange: any = req.query.timeRange;
-    if (!timeRange) sendStatusMsg(400, false, "No timeRange passed.", res);
-    if (timeRange !== "day" && timeRange !== "week" && timeRange !== "allTime")
-      sendStatusMsg(
-        400,
-        false,
-        "timeRange passed is not day / week / month / allTime.",
-        res
-      );
+      //Check if timeRange passed and in correct format
+      const timeRange: any = req.query.timeRange;
+      if (!timeRange) sendStatusMsg(400, false, "No timeRange passed.", res);
+      if (
+        timeRange !== "day" &&
+        timeRange !== "week" &&
+        timeRange !== "allTime"
+      )
+        sendStatusMsg(
+          400,
+          false,
+          "timeRange passed is not day / week / month / allTime.",
+          res
+        );
 
-    let orderByField = "totalGamesWon";
-    if (timeRange === "day") orderByField = "gamesWonDay";
-    else if (timeRange === "week") orderByField = "gamesWonWeek";
-    numOfPlayers = Number.parseInt(numOfPlayers);
-    const topPlayers: any = [];
+      let orderByField = "totalGamesWon";
+      if (timeRange === "day") orderByField = "gamesWonDay";
+      else if (timeRange === "week") orderByField = "gamesWonWeek";
+      numOfPlayers = Number.parseInt(numOfPlayers);
+      const topPlayers: any = [];
 
-    return usersCol
-      .orderBy(orderByField, "desc")
-      .limit(numOfPlayers)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          topPlayers.push(doc.data());
+      return usersCol
+        .orderBy(orderByField, "desc")
+        .limit(numOfPlayers)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            topPlayers.push(doc.data());
+          });
+        })
+        .then(() => {
+          sendData(200, "topPlayers", topPlayers, res);
+        })
+        .catch((err) => {
+          console.log(err);
+          sendStatusMsg(500, false, "Query failed.", res);
         });
-      })
-      .then(() => {
-        sendData(200, "topPlayers", topPlayers, res);
-      })
-      .catch((err) => {
-        console.log(err);
-        sendStatusMsg(500, false, "Query failed.", res);
-      });
+    });
   });
 
 //Required gameId as query and data with uid / username / message
 export const addChatMessage = functions
   .region("asia-southeast2")
   .https.onRequest((req, res) => {
-    //Check if gameId passed
-    const gameId: any = req.query.gameId;
-    if (!gameId) sendStatusMsg(400, false, "No gameId passed.", res);
+    corsHandler(req, res, () => {
+      //Check if gameId passed
+      const gameId: any = req.query.gameId;
+      if (!gameId) sendStatusMsg(400, false, "No gameId passed.", res);
 
-    //Check if message passed
-    const msg = req.body.message;
-    if (!msg) sendStatusMsg(400, false, "No message passed.", res);
+      //Check if message passed
+      const msg = req.body.message;
+      if (!msg) sendStatusMsg(400, false, "No message passed.", res);
 
-    const uid = req.body.uid ? req.body.uid : -1;
-    const username = req.body.username ? req.body.username : "anonymous";
-    const data = {
-      uid: uid,
-      username: username,
-      message: msg,
-      createdAt: admin.firestore.Timestamp.now(),
-    };
+      const uid = req.body.uid ? req.body.uid : -1;
+      const username = req.body.username ? req.body.username : "anonymous";
+      const data = {
+        uid: uid,
+        username: username,
+        message: msg,
+        createdAt: admin.firestore.Timestamp.now(),
+      };
 
-    return gamesRef
-      .child(gameId)
-      .push()
-      .set(data)
-      .then(() => {
-        sendData(200, "data", data, res);
-      })
-      .catch((err) => {
-        console.log(err);
-        sendStatusMsg(500, false, "Error while adding message.", res);
-      });
+      return gamesRef
+        .child(gameId)
+        .push()
+        .set(data)
+        .then(() => {
+          sendData(200, "data", data, res);
+        })
+        .catch((err) => {
+          console.log(err);
+          sendStatusMsg(500, false, "Error while adding message.", res);
+        });
+    });
   });
 
 //Required gameId as query
 export const deleteGameChat = functions
   .region("asia-southeast2")
   .https.onRequest((req, res) => {
-    //Check if gameId passed
-    const gameId: any = req.query.gameId;
-    if (!gameId) sendStatusMsg(400, false, "No gameId passed.", res);
+    corsHandler(req, res, () => {
+      //Check if gameId passed
+      const gameId: any = req.query.gameId;
+      if (!gameId) sendStatusMsg(400, false, "No gameId passed.", res);
 
-    return gamesRef
-      .child(gameId)
-      .remove()
-      .then(() => {
-        sendData(200, "gameId", gameId, res, "Game chat messages deleted");
-      })
-      .catch((err) => {
-        console.log(err);
-        sendStatusMsg(404, false, "No game room found with given gameId.", res);
-      });
+      return gamesRef
+        .child(gameId)
+        .remove()
+        .then(() => {
+          sendData(200, "gameId", gameId, res, "Game chat messages deleted");
+        })
+        .catch((err) => {
+          console.log(err);
+          sendStatusMsg(
+            404,
+            false,
+            "No game room found with given gameId.",
+            res
+          );
+        });
+    });
   });
 
 //Require timeRange (day / week) as query
 export const resetAllUsersGamesWon = functions
   .region("asia-southeast2")
-  .https.onRequest(async (req, res) => {
-    //Check if timeRange passed and in correct format
+  .https.onRequest((req, res) => {
+    corsHandler(req, res, async () => {
+      //Check if timeRange passed and in correct format
     const timeRange = req.query.timeRange;
     if (!timeRange) sendStatusMsg(400, false, "No timeRange passed.", res);
     if (timeRange !== "day" && timeRange !== "week")
@@ -279,6 +303,7 @@ export const resetAllUsersGamesWon = functions
         console.log(err);
         sendStatusMsg(500, false, "Error while commiting batch", res);
       });
+    })
   });
 
 function sendStatusMsg(
